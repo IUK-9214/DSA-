e99e100afa33b4985acd3aaa2dd223db546d0a50
IBAD ULLAH KHAN <ibad50008@gmail.com>
2024-11-11 11:04:26 +0500

    practice is start

---
+++ practice of deletion from the array.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main (){
+	//using the DSA concept
+	//here we can create a program 
+	//for deletion from the array
+int numb[N];
+int N;
+cout<<"Enter the size of the array"<<endl;
+for(int i=0;i>)
+
+	
+	return 0;
+}
