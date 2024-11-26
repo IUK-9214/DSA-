b66489b1fef73ea10bc7edfe88135a88cb5e86da
IBAD ULLAH KHAN <ibad50008@gmail.com>
2024-11-26 23:22:14 +0500

    practice done

--- PRACTICE STACK.cpp
+++ PRACTICE STACK.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<stack>
 using namespace std;
 int main(){
 	//we will create a stack l
@@ -6,7 +7,21 @@ int main(){
 	 //pop()
 	 //push()
 	 //isfull()
-	 
+	 stack<int>a;
+	 a.push(3);
+	 a.push(6);
+	 a.push(8);
+	 a.push(6);
+//	 cout<<a.top();
+//	 if(!a.empty()){
+//	 	a.pop();
+//	 	cout<<" "<<a.top();
+//	 }
+	 while(!a.empty()){
+	 	cout<<a.top()<<" ";
+	 	a.pop();
+	 	
+	 }
 	
 	return 0;
 }
