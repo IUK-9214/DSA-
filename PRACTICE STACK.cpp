3f4ab1b5173f885740704bf214f60b7d4b5d449e
IBAD ULLAH KHAN <ibad50008@gmail.com>
2024-11-26 09:55:55 +0500

    compelete
    deletion is completed in the array after

---
+++ PRACTICE STACK.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	//we will create a stack l
+	 //top ()
+	 //pop()
+	 //push()
+	 //isfull()
+	 
+	
+	return 0;
+}
--- practice of deletion from the array.cpp
+++ practice of deletion from the array.cpp
@@ -4,10 +4,39 @@ int main (){
 	//using the DSA concept
 	//here we can create a program 
 	//for deletion from the array
+	int N,index;
 int numb[N];
-int N;
 cout<<"Enter the size of the array"<<endl;
-for(int i=0;i>)
+cin>>N;
+cout<<"Enter the element of the array"<<endl;
+for(int i=0;i<N;i++){
+	cin>>numb[i];
+}
+cout<<"Your array elemnt are "<<endl;
+for(int i=0;i<N;i++){
+	cout<<numb[i]<<" ";
+}
+cout<<endl;
+cout<<"enter the index number to delete the element from the array"<<endl;
+cin>>index;
+
+if (index>0||index<N){
+	
+	for(int j=index;j<=N;j++){
+		numb[index]=numb[index+1];
+	index++;
+	}
+	N--;
+	cout<<"aftter deletion the element"<<endl;
+	for(int j=0;j<N;j++){
+		cout<<numb[j]<<" ";
+		
+	}
+}
+else{
+	cout<<"invalide index"<<endl;
+}
+
 
 	
 	return 0;
