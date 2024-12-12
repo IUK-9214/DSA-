fa76b0bb4265d0ee2fe15f9834a7062bca2cbe20
IBAD ULLAH KHAN <ibad50008@gmail.com>
2024-12-12 16:06:31 +0500

    here i use a linaer search tech

--- linear searching in the array.cpp
+++ linear searching in the array.cpp
@@ -1,16 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-	int array[6]={1,23,4,5,3,6},loc;
-	int se=23;
-	for (int i=0;i<6;i++){
-		if (array[i]==se){
-			loc=i;
-		}
+int main( ) 
+{
+int a[6]={6,8,9,7,5,4}, n=6 ,se;
+int loc=-1,i=0;
 
-	}
-	cout<<"the location of the element in the array : "<<loc<<endl;
-	cout<<"the number is : "<<array[loc]<<endl;
+se=7;
+while(i<n )
+{
+i++;
+if( a[i] == se)
+{
+loc=i;
+}
+}
+cout<<"item found at location"<<loc;
 	
 	return 0;
 }
