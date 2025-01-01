30f2123d055e62c29d0d15de632cdbb4e1419fd0
IBAD ULLAH KHAN <ibad50008@gmail.com>
2025-01-01 09:54:04 +0500

    single linked list
    here i use the single linked list concept to create a single linked list

--- linked list.cpp
+++ linked list.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
 using namespace std;
-int main (){
-	
-	
-	return 0;
+
+struct Node
+{
+    int data; 
+    Node *next;      
+};
+
+int main()
+{ 
+   Node *node1 = new Node; 
+  Node *node2 = new Node; 
+ Node *node3 = new Node;
+ Node *nullptr;  
+
+    node1->data = 10;
+    node2->data = 20;
+    node3->data = 30;
+    
+    node1->next = node2;  
+    node2->next = node3;  
+    node3->next = nullptr; 
+    
+    Node *current = node1;  
+    while (current != nullptr)
+    {
+        cout << current->data << " -> ";
+        current = current->next; 
 }
+    cout << "NULL" << endl;
+
+    return 0;
+}
+
