2dba6939a2ea9ec5ab629559b22058f38b029352
IBAD ULLAH KHAN <ibad50008@gmail.com>
2025-03-18 08:45:47 +0500

    exercsie

--- practice-02.cpp
+++ practice-02.cpp
@@ -1,12 +1,20 @@
 #include<iostream>
 using namespace std;
-class{
+class rectangle{
 public:
+int area(int l, int b){
 
+   int  area_1=l*b;
+    return area_1;
+}
 
 };
 
 int main (){
-
+rectangle r1;
+int area1=r1.area(4,5);
+cout<<"the area of the first rectangle is : "<<area1<<endl;
+int area2=r1.area(5,8);
+cout<<"the area of the second rectangle is : "<<area2<<endl;
     return 0;
 }
